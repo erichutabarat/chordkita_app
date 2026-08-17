7650e4f5a160846b2f6f472f2f7fe85c6693ab00
erichutabarat <handsomeware.sec@gmail.com>
2026-08-17 15:14:39 +0700

    feat(favorite_screen): add header and list of chord item

--- lib/features/home/presentation/screens/favorite_screen.dart
+++ lib/features/home/presentation/screens/favorite_screen.dart
@@ -1,3 +1,5 @@
+import 'package:chordkita/features/home/data/samples/chord_sample.dart';
+import 'package:chordkita/features/home/presentation/widgets/chordsong_item.dart';
 import 'package:flutter/material.dart';
 
 class FavoriteScreen extends StatefulWidget {
@@ -11,16 +13,28 @@ class _FavoriteScreenState extends State<FavoriteScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(8),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Icon(Icons.music_note, size: 64, color: Colors.amber),
-            SizedBox(height: 16),
-            Text(
-              'Your chord library is getting ready...',
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+            // Header
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.amber, width: 2),
+                color: Colors.amber.withOpacity(0.2),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: const Text(
+                'Favorite Chords',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
             ),
+            const SizedBox(height: 18),
+            // render here
+            ...topSongs.map((song) => ChordsongItem(data: song)),
           ],
         ),
       ),
