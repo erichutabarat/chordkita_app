8c3a8e50e1a178ba5e4e6bc74a6e0108c73f2abe
erichutabarat <handsomeware.sec@gmail.com>
2026-08-22 16:15:50 +0700

    feat(chord, song): create a sample for song and its chord

---
+++ lib/features/chord/data/samples/song_samples.dart
@@ -0,0 +1,8 @@
+import 'package:chordkita/features/chord/domain/entities/song.dart';
+
+final Song songSample = Song(
+  id: 1,
+  title: "Asal Kau Bahagia",
+  artist: "Armada",
+  chordkey: "C",
+);
